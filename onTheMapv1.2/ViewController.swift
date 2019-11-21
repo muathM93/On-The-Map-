1e6adbf96fc1b2a083ebc74c9945295b53131905
Muath Boubaid <mboubaid@channels.com.sa>
2019-11-21 21:05:50 +0300

    Initial Commit

---
+++ onTheMapv1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onTheMapv1.2
+//
+//  Created by Muath Mohammed on 24/03/1441 AH.
+//  Copyright © 1441 MuathMohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
